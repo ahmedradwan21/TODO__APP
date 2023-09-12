b6db2e574b5c825c3e94a5f7ccd3904ef6601cc4
Ahmed Tarek Radwan (ATR) <100035760+ahmedradwan21@users.noreply.github.com>
2023-09-12 15:34:08 +0300

    Hello <=>

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -46,7 +46,7 @@ REST_FRAMEWORK = {
 import datetime
 
 JWT_AUTH = {
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=24),
 }
 
 MIDDLEWARE = [
